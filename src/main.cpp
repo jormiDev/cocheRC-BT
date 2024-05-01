5093260ce57e6f35d05f6d25089502076ca6fa55
pablo <79108100+jormiDev@users.noreply.github.com>
2024-05-01 20:07:15 +0200

    ble 002
    test conexion con ejemplo

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
 #include <ArduinoBLE.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <WiFi.h>
 
 #include <jormazLib.h>
 #include <constantes.h>
@@ -12,36 +15,38 @@
 jormiDepura mensaje;
 String(cadenaLeidaMITapp);
 
-BLEService cocheService("19B10000-E8F2-537E-4F6C-D104768A1214");                                            // create service
-BLEByteCharacteristic cocheCharacteristic("19B10000-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);      // carcteriticas Read / Write
+BLEService cocheService("19B10000-E8F2-537E-4F6C-D104768A1214");											// create service
+BLEByteCharacteristic cocheCharacteristic("19B10000-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);		// carcteriticas Read / Write
 
 /*
  * ********   S E T U P   ***************
  */
 
-void setup()
-{
-  // Serial init
-  Serial.begin(9600);
-  while (!Serial);
+void setup(){
 
-  //BLE init
-  if (!BLE.begin()){
+// Serial init
+Serial.begin(9600);
+while (!Serial);
+
+//BLE init
+if (!BLE.begin()){
     Serial.println("starting Bluetooth® Low Energy module failed!");
     while (1);
-  }
-  BLE.setLocalName("cocheMITapp");                      // set the local name peripheral advertises
-  BLE.setAdvertisedService(cocheService);               // set the UUID for the service this peripheral advertises:
-  cocheService.addCharacteristic(cocheCharacteristic);  // add the characteristics to the service
-  BLE.addService(cocheService);                         // add the service
-  cocheCharacteristic.writeValue(0);                    // valor inicial caracteristica
-  BLE.advertise();                                      // start advertising
-  Serial.println("Bluetooth® device active, waiting for connections...");
-
-  // fin Setup
-  mensaje.texto(" inicio loop ");
-  delay(1000);
 }
+BLE.setLocalName("UNOr4WifiPradera");						// set the local name peripheral advertises
+BLE.setAdvertisedService(cocheService);						// set the UUID for the service this peripheral advertises:
+cocheService.addCharacteristic(cocheCharacteristic);		// add the characteristics to the service
+BLE.addService(cocheService);								// add the service
+cocheCharacteristic.writeValue(0);							// valor inicial caracteristica
+BLE.advertise();											// start advertising
+Serial.println("Bluetooth® device active, waiting for connections...");
+
+// fin Setup
+mensaje.texto(" inicio loop ");
+delay(1000);
+
+
+}//	setup
 
 
 
@@ -49,14 +54,14 @@ void setup()
  * ********   L O O P   ***************
  */
 
-void loop()
-{
-  BLE.poll(); // poll for Bluetooth® Low Energy events
+void loop(){
 
-  mensaje.vble("MIT app :      ", cocheCharacteristic.value());
+BLE.poll();			// poll for Bluetooth® Low Energy events
+
+mensaje.vble("MIT app :      ", cocheCharacteristic.value());
   
 
 
-  delay(3000);
+delay(3000);
 
-  } // loop
+}//	loop
