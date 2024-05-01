26708d2f514c909642306150c1b3847eaa98a2b3
pablo <79108100+jormiDev@users.noreply.github.com>
2024-05-01 13:40:06 +0200

    ble 01
    pasado todo a carpeta test e inicializado para conexion con BLE

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,19 @@
 #include <Arduino.h>
+#include <ArduinoBLE.h>
 
 #include <jormazLib.h>
-#include "TimerOne.h"
-#include "HCSR04.h"
-#include "Servo.h"
-
 #include <constantes.h>
-#include "CBasicoMotor.hpp"
-#include "CBasicoRemoto.hpp"
-#include "CBasicoSensorDist.hpp"
-#include "logicaRC.hpp"
 
  
 /*
  * Objetos y variables
  */
 
-
 jormiDepura mensaje;
-int maquinaEstados;
+String(cadenaLeidaMITapp);
 
-// CBasicoMotor.hpp
-volatile unsigned int pulsosDerecha;                        // Contador pulsos lado derecho
-volatile unsigned int pulsosIzquierda;                      // Contador pulsos lado izquierdo
-float rpmDerecha;                                           // RPM motor derecho
-float rpmIzquierda;                                         // RPM motor izquierdo
-jormiL293D driverMotor(PIN_IN1, PIN_IN2, PIN_IN3, PIN_IN4); // Motor coche
-int velDeseadaDerecha;                                      // Valor deseado del motor derecho -255..0..255
-int velActualDerecha;                                       // Valor actual motor derecho -255..0..255
-int velDeseadaIzquierda;                                    // Valor deseado del motor izquierdo -255..0..255
-int velActualIzquierda;                                     // Valor actual motor izquierdo -255..0..255
-
-// CBasicoSensorDist.hpp
-int distValores[5];           // Array de distancias 0 .. 180
-float tiempoActual;           // tiempo actual
-float tiempoPrevio;           // tiempo de la ultima lectura sensor distancia
-HCSR04 distHCSR04 (PIN_HCSR04_TRIGGER, PIN_HCSR04_ECHO);      //HC-SR04
-Servo myservo;                //SG90
-
-// CBasicoRemoto.hpp
-char comando;                 // Comando leido de la appMIT inventor
-
-// CBasicoTes
+BLEService cocheService("19B10000-E8F2-537E-4F6C-D104768A1214");                                            // create service
+BLEByteCharacteristic cocheCharacteristic("19B10000-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);      // carcteriticas Read / Write
 
 /*
  * ********   S E T U P   ***************
@@ -49,23 +21,25 @@ char comando;                 // Comando leido de la appMIT inventor
 
 void setup()
 {
-
+  // Serial init
   Serial.begin(9600);
+  while (!Serial);
 
-  maquinaEstados = 0;
+  //BLE init
+  if (!BLE.begin()){
+    Serial.println("starting Bluetooth® Low Energy module failed!");
+    while (1);
+  }
+  BLE.setLocalName("cocheMITapp");                      // set the local name peripheral advertises
+  BLE.setAdvertisedService(cocheService);               // set the UUID for the service this peripheral advertises:
+  cocheService.addCharacteristic(cocheCharacteristic);  // add the characteristics to the service
+  BLE.addService(cocheService);                         // add the service
+  cocheCharacteristic.writeValue(0);                    // valor inicial caracteristica
+  BLE.advertise();                                      // start advertising
+  Serial.println("Bluetooth® device active, waiting for connections...");
 
-  distanciaSetup();   // HC-SR04 + SG90
-  motorSetup();       // motores
- // remotoSetup();
-
-
-
-
-  tiempoPrevio = millis();
-  tiempoActual = tiempoPrevio;
-
-  // Fin setup
-  mensaje.texto(" loop ");
+  // fin Setup
+  mensaje.texto(" inicio loop ");
   delay(1000);
 }
 
@@ -77,77 +51,12 @@ void setup()
 
 void loop()
 {
+  BLE.poll(); // poll for Bluetooth® Low Energy events
 
-  mensaje.texto("inicio loop");
-
-  tiempoActual = millis();                          // tiempo del sistema
-  delay(2000);
-
-  //comando = remotoLeer();                           // leer el buffer BT y almacenar el comando
-
-
-  // switch (maquinaEstados)
-  // /*
-  //   0     tiempo transcurrido <= TIEMPO_PERIODO
-  //   1     tiempo transcurrido > TIEMPO_PERIODO        
-  //   2     evaluar velocidad según distancia frontal
-  //   3     velocidad > 0
-  //   4     velocidad = 0 (STOP)
-
-
-  // */
-
-
-  // {
-  // case 0:
-  //   // Mira si ha transcurrido "tiempoPeriodo"		->	1
-  //   if ((tiempoActual - tiempoPrevio) > TIEMPO_PERIODO)
-  //   {
-  //     maquinaEstados = 1;
-  //     tiempoPrevio = tiempoActual;
-  //   }
-  //   break;
-
-  // case 1:
-  //   // Medir distancia (frontal)			-> 	2
-  //   distanciaUpdate();
-  //   maquinaEstados = 2;
-  //   break;
-
-  // case 2:
-  //   // Mantener dirección		-> 	3
-  //   // Cambio de dirección		->	4
-  //   maquinaEstados = logicaBasica();
-  //   break;
-
-  // case 3:
-  //   // Actualizar motores		-> 	0
-  //   motorSerial();
-  //   motorUpdate();
-  //   maquinaEstados = 0;
-  //   break;
-
-  // case 4:
-  //   // Calcular nueva dirección
-  //   // Aplicar giro				->	5
-  //   distanciaUpdate();
-  //   motorGiro(logicaGiro());
-  //   maquinaEstados = 5;
-  //   break;
-
-  // case 5:
-  //   // Empezar avance lento		->	0
-  //   velDeseadaDerecha = VELOCIDAD_BAJA;
-  //   velDeseadaIzquierda = VELOCIDAD_BAJA;
-  //   motorSerial();
-  //   motorUpdate();
-  //   maquinaEstados = 0;
-  //   break;
-
-  // default:
-  //   maquinaEstados = 0;
-  //   break;
-
-  // } // maquinaEstados
+  mensaje.vble("MIT app :      ", cocheCharacteristic.value());
   
-} // loop
+
+
+  delay(3000);
+
+  } // loop
