5c40241f016d9ea1ad2d5ed3eb57ca84f0d7cdc0
pablo <79108100+jormiDev@users.noreply.github.com>
2024-08-25 12:48:16 +0200

    Update cocheMaqEstados.cpp

--- src/cocheMaqEstados.cpp
+++ src/cocheMaqEstados.cpp
@@ -11,44 +11,46 @@
  * 	* * * * * Funciones * * * * *
  */
 
-// modo manual - app_STOP
+// modo manual/auto - app_STOP
 void manual_STOP(){
-
+    motorDC_stop();
 }
 
 // modo manual - app_FWD
 void manual_FWD(){
-
+    motorDC_fwd(VELOCIDAD_MEDIA);
 }
 
 // modo manual - app_AFT
 void manual_AFT(){
-
+    motorDC_aft(VELOCIDAD_MEDIA);
 }
 
 // modo manual - app_IZQ
 void manual_IZQ(){
-
+    motorDC_A_fwd(VELOCIDAD_MEDIA);
+    motorDC_B_fwd(VELOCIDAD_BAJA);
 }
 
 // modo manual - app_DCHA
 void manual_DCHA(){
-
+    motorDC_A_fwd(VELOCIDAD_BAJA);
+    motorDC_B_fwd(VELOCIDAD_MEDIA);
 }
 
 // modo manual - app_90IZQ
 void manual_90IZQ(){
-    
+    motorDC_90_IZQDA();
 }
 
 // modo manual - app_90DCHA
 void manual_90DCHA(){
-
+    motorC_90_DRCHA();
 }
 
 // modo manual - app_180
 void manual_180(){
-
+    motor_DC_180();
 }
 
 //  0
@@ -78,11 +80,11 @@ void maqEstados_QUIT_APP(){
 //  4
 // parar todo + analizar escenario + inicir conduccion automatica
 void maqEstados_MODO_AUTO(){
-
+    maquinaEstados = ME_MODO_AUTO;
 }
 
 //  5
 // parar todo + esperar comnandos
 void maqEstados_MODO_MANUAL(){
-
+    maquinaEstados = ME_MODO_MANUAL;
 }
